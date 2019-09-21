d5dbe58afab84c1ce16240239183b38a45207be3
Jazz Love <mjzzjazzy@ksu.edu>
2019-09-20 21:30:53 -0500

    added kelvin conversion

--- temperature.sh
+++ temperature.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
-# read in a temperature from Fahrenheit to Celsius
+# read in a temperature from Fahrenheit to Celsius and Kelvin
 
-echo -n "Please enter a temperature -> "
+echo -n "Please enter a temperature (Fahrenheit)-> "
 read temp
 
 bc <<- EOF
@@ -12,6 +12,10 @@ temp_conv_32 = ($temp - 32)
 temp_conv_5 = (temp_conv_32*5)
 
 temp_conv = (temp_conv_5/9)
+
+temp_k = (temp_conv + 273.15)
+
 print temp_conv, "\n"
+print temp_k, "\n"
 EOF
 
